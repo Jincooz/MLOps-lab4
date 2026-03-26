eceef96917d95deca81138b4c6c5e0199cf54331
Vitalii Koptsov <shaddyreaper@gmail.com>
2026-03-26 12:40:03 +0200

    Added fake_client to boto3 mock

--- conftest.py
+++ conftest.py
@@ -1,5 +1,16 @@
 import sys
 from unittest.mock import MagicMock
+import types
 
-sys.modules["boto3"] = MagicMock()
+try:
+    import boto3 
+except ModuleNotFoundError:
+    fake_boto3 = types.SimpleNamespace()
+
+    def fake_client(*args, **kwargs):
+        raise RuntimeError("boto3 is not installed (mocked in tests)")
+
+    fake_boto3.client = fake_client
+    sys.modules["boto3"] = fake_boto3
+
 sys.modules["prometheus_client"] = MagicMock()
